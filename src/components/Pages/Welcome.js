ad78a138c75330bb51329698e653da396c6ef502
Azril Nazli Alias <azril.nazli@gmail.com>
2022-05-08 23:03:59 +0800

    frontpage

--- src/components/Pages/Welcome.js
+++ src/components/Pages/Welcome.js
@@ -8,8 +8,9 @@ const Welcome = (props) => {
 
         return (
             <div className='container container-fluid bg-light rounded p-3'>           
-                <div className="ratio ratio-1x1">
-                        <iframe className="embed-responsive-item" src="/pdf/syarat.pdf"></iframe>
+                <div className="ratio ratio-16x9">
+                        {/* <iframe className="embed-responsive-item" src="/pdf/syarat.pdf"></iframe> */}
+                        <img src="/img/angkasapuri.jpg" className="img-fluid" />
                 </div>
             </div>
         );
