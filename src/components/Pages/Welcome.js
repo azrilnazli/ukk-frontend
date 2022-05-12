abccd8b9a6a13a6654185ae4f78a4e40ec9351b7
Azril Nazli Alias <azril.nazli@gmail.com>
2022-05-12 13:38:34 +0800

    logo depan

--- src/components/Pages/Welcome.js
+++ src/components/Pages/Welcome.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import apiClient from '../../services/api';
 import { Redirect } from 'react-router-dom';
+import logo from './img/angkasapuri.jpg'
 
 const Welcome = (props) => {
 
@@ -10,7 +11,7 @@ const Welcome = (props) => {
             <div className='container container-fluid bg-light rounded p-3'>           
                 <div className="ratio ratio-16x9">
                         {/* <iframe className="embed-responsive-item" src="/pdf/syarat.pdf"></iframe> */}
-                        <img src="/img/angkasapuri.jpg" className="img-fluid" />
+                        <img src={logo} className="img-fluid" />
                 </div>
             </div>
         );
