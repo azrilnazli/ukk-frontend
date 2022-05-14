dc10ff2b07afd86b727e25d8552f33964fd0249b
Azril Nazli Alias <sales@stream.my>
2022-05-14 18:34:10 +0800

    pop up

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -49,8 +49,8 @@ const Dashboard = () => {
     return (
 
       
-    // <div className='container container-fluid bg-light rounded p-3'>
-    // <Alert />
+    <div className='container container-fluid bg-light rounded p-3'>
+    <Alert />
     //   <div className="alert alert-warning text-danger" role="alert">
     //   <i className="fa fa-exclamation-triangle"></i> MAKLUMAN: Pendaftaran syarikat bagi pembekalan swasta & sambung siri 2023 akan tamat pada Jam 11:59 malam, 14/Mei/2022
     //   </div>
