f1554e8b5a97574886842db9548eadf43c93de05
Azril Nazli Alias <azril.nazli@gmail.com>
2022-05-18 14:40:27 +0800

    console log admin

--- src/components/MyProposal/Video.js
+++ src/components/MyProposal/Video.js
@@ -7,7 +7,12 @@ import {Modal, Button, Form} from 'react-bootstrap';
 const collect = require('collect.js'); 
 
 
+
 const Video = ({proposal_id,tender_id}) => {
+
+    // console.log('********************************')
+    // console.log('welcome to Video Check System ')
+    // console.log('********************************')
 
     const [file,setFile] = useState('');
     const [filename,setFilename] = useState('Choose file');
@@ -28,60 +33,72 @@ const Video = ({proposal_id,tender_id}) => {
 
     const getCurrentVideo = () => {
 
+        console.log('.............................')
+        console.log('Initiate Get Current Video Check')
+        console.log('.............................')
         // get current video id
+        console.log('check : get_video from server')
         apiClient.get(`/api/proposal/${proposal_id}/get_video`) // axios call to txt file
         .then((response) => {
             //console.log(response) 
-
+            console.log('check : video_id exists is TRUE ?')
             if(response.data.exists === true){
-                console.log('showing video with id ' + response.data.video_id)
+                console.log('result : video_id exists with id ' + response.data.video_id)
                 setVideoId(response.data.video_id)
                 setShowVideo(true)
 
-                if(response.data.video_id){
+                console.log('check : video_id not null ?')
+                if(response.data.video_id !== 0){
                     // check existing conversion status
                     //console.log('video is is ' + response.data.video_id)
+                    console.log('result : video_id not null with id ' + response.data.video_id)
+                    console.log('check : video_id is processing ?')
                     apiClient.get(`/api/video/${response.data.video_id}/conversion_progress`)
                     .then((response) => {
-                        console.log(response)  
+                        console.log('is processing ? got response...')  
                         if(response.data.converting === true){ // is converting
                           
                             setShowVideo(false)
                             setUploaded(true); // setter 
-                            console.log('checked : is converting')
+                            console.log('result : is_processing is true')
+                        }else{
+                            console.log('result : is_processing is false')
                         }          
                     })
                     .catch((e) => {
                       
                         console.log(e.error);
-                        console.log("Error");
+                        console.log("Error : processing check");
                         setConversionPercentage(0); // set counter to zero
                     });
-                } // check videoID
+                }else {
+                    console.log('video_id is null')
+                }// check videoID
 
                 // check is video playable
-                if(response.data.video_id){
+                console.log('check : if video is_playable ?')
+                if(response.data.video_id != 0){
                     // check existing conversion status
-                    console.log('check video playable ' + response.data.video_id)
+                    console.log('result : got response ... video_id is :' + response.data.video_id)
                     apiClient.get(`/api/video/${response.data.video_id}/is_playable`)
                     .then((response) => {
-                        console.log(response)  
+                         
                         if(response.data.is_playable == true){ // is converting
-                            console.log('is playable : yes')
+                            console.log('result : is playable : yes')
                             setIsVideoPlayable(true)
                         } else {
-                            console.log('is playable : no')
+                            console.log('result : is playable : no')
                         }      
                     })
                     .catch((e) => {
                         console.log(e.error);
-                        console.log("Error");
+                        console.log("Error : playable check");
                         setConversionPercentage(0); // set counter to zero
                     });
                 } // check videoID
 
             } else {
-                console.log('User has no video')
+                console.log('result : user has no video')
             }         
         })
         .catch((e) => {
@@ -95,22 +112,35 @@ const Video = ({proposal_id,tender_id}) => {
     React.useEffect(() => getCurrentVideo(), []); // check if video was uploaded
 
     const getVideoProgress = () => {
-        
+        console.log('.............................')
+        console.log('Initiate Video Progress Check')
+        console.log('.............................')
+        console.log('check : if videoId was set ?')
         if(videoId !== '') { //if videoId is present 
             
+            console.log('result : videoId is set :' + videoId)
+            setIsDisabled(true)
+            setShowVideo(false)
+            
+            
             let timer = setInterval(() => { // timer is setInterval() id , need to clear if conversionPercentage == 100
+                console.log('check : get conversion_process')
+               
                 apiClient.get(`/api/video/${videoId}/conversion_progress`) // axios call to txt file
                 .then((response) => {
                 
-                        
+                    console.log('result :  conversion_process exist')
+                    console.log('check : if converting is true ?')
+
                         if(response.data.converting == true){
-                           
-                            setShowVideo(false)
+                            console.log('result : is converting')
+                       
                             setConversionPercentage(response.data.progress); // setter 
-                            console.log('is converting')
+                        
                         } else {
-                            console.log('done converting')
+                            console.log('result : not converting')
                             setSystemMsg('Your video was successfully uploaded and processed.')
+                            setIsDisabled(false)
                             setShowVideo(true)
                             setIsVideoPlayable(true)
                            
@@ -120,13 +150,16 @@ const Video = ({proposal_id,tender_id}) => {
           
                 })
                 .catch((e) => {
-                    console.log("Error");
+                    console.log("Error : conversion_progress");
                    
                     setConversionPercentage(0); // set counter to zero
                     clearInterval(timer)
                 });
-            }, 1000); // update evey 1 sec
-            
+            }, 2000); // update evey 1 sec
+
+  
+        } else {
+            console.log('result : videoId is not being set')
         }
     }
 
@@ -134,6 +167,9 @@ const Video = ({proposal_id,tender_id}) => {
     React.useEffect(() => getVideoProgress(), [uploaded,videoId]); // only run useEffect when videoId is changed
 
     const handleSubmit = (e) => {
+        console.log('.............................')
+        console.log('Initiate Video Upload Check')
+        console.log('.............................')
         e.preventDefault();
         
         setErrors('')
@@ -162,6 +198,7 @@ const Video = ({proposal_id,tender_id}) => {
         formData.append('file', file) // selected file
 
         setSystemMsg('Your video is being uploaded')
+        console.log('check : user begin to upload video')
         // axios 
         apiClient({
             method: "post",
@@ -183,6 +220,7 @@ const Video = ({proposal_id,tender_id}) => {
 
             
         }).then(response => {
+            console.log('result : video uploaded successfully')
             setUploadPercentage(0)
             setSystemMsg('Your video was uploaded successfully. Now sent for encoding. Please wait...')
             
@@ -196,11 +234,11 @@ const Video = ({proposal_id,tender_id}) => {
       
         }).catch(error => {
             if (error.response.status === 500) {
-                console.log('Error 500'); 
+                console.log('Error 500 : upload video'); 
                 setErrors('Server Error : 500');
             }
             if (error.response.status === 422) {
-                console.log(error.response)
+                console.log('Error : upload video validation error');
                 setErrors(error.response.data.errors.file[0]);
             }
             
@@ -208,6 +246,7 @@ const Video = ({proposal_id,tender_id}) => {
     }
 
     function ShowVideoPlayer() {
+
         const [show, setShow] = useState(false);
       
         const handleClose = () => setShow(false);
@@ -242,11 +281,6 @@ const Video = ({proposal_id,tender_id}) => {
 
     return (
         <>
-    
-        
-
-
-
             <form onSubmit={handleSubmit}>
                
                 <div className='row' >
