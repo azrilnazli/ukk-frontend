be219ce08b424ab6aa97621d87b087a51ac18732
Azril Nazli Alias <azril.nazli@gmail.com>
2022-05-19 16:31:13 +0800

    check user is approved

--- src/components/Tender/SambungSiri.js
+++ src/components/Tender/SambungSiri.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import apiClient from '../../services/api';
 import Detail from './Detail';
+import ErrorMsg from './ErrorMsg';
 const collect = require('collect.js'); 
 
 
 const TenderSambungSiri = () => {
 
+    const [error,setError] = React.useState('')
+    const [title,setTitle] = React.useState('')
     const [tenders, setTenders] = React.useState([]);
     const getTenderList = () => {
+
+        // need to check user.company.is_approved = true
         apiClient.get('/api/tenders/sambung_siri')
         .then(response => {
-           // console.log(response.data)
+            console.log(response.data)
             setTenders(response.data.tenders)
         })
-        .catch(error => console.error(error));
+        .catch(error => { 
+            console.error(error.response.data)
+            if (error.response.status === 422) {
+                setTitle(error.response.data.title); 
+                setError(error.response.data.message);
+               
+            }
+        });
     }
     React.useEffect(() => getTenderList(), []); 
 
@@ -22,7 +34,11 @@ const TenderSambungSiri = () => {
     );
 
     return (
+        <>
+        { error ? <ErrorMsg title={title} message={error} /> :
         <div>{tenderList}</div>
+        }
+        </>
     );
 };
 
