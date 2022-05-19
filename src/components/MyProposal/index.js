2e8bdd3261b04ccd9ea3f94e24ab6fc916f1c5c5
Azril Nazli Alias <sales@stream.my>
2022-05-19 20:33:56 +0800

    My Proposal

--- src/components/MyProposal/index.js
+++ src/components/MyProposal/index.js
@@ -1,16 +1,49 @@
 import React , { useState }  from 'react';
 import Pdf from './Pdf';
 import Video from './Video';
+import apiClient from '../../services/api';
+import Detail from './Detail';
 
 
 const MyProposal = () => {
 
+    const [isPending, setIsPending] = useState(false)
+    const [proposals, setProposals] = useState([false])
+
+    const getProposal = () => {
+        setIsPending(true)
+        console.log('.............................')
+        console.log('Initiate Get Proposal from Server')
+        console.log('.............................')
+        // get current video id
+        console.log('check : get_video from server')
+        apiClient.get('/api/proposal/my_proposal') // axios call to server
+        .then((response) => {
+            setIsPending(false)
+            console.log('result: got reponse... fetching data')     
+            console.log(response.data)
+            setProposals(response.data.proposals)
+        })
+        .catch((e) => {
+            setIsPending(false)
+            console.log(e.error);
+            console.log("Error getting data");
+        });
+    }
+    React.useEffect(() => getProposal(), []); // GET request to server
+
+    const proposalList = proposals.map((proposal) => 
+        <Detail tender={proposal.tender} />
+    );
 
     return (
-        <>
-            <Video />
-            {/* <Pdf /> */}
-        </>
+        <div className='container container-fluid bg-light rounded p-3 col-md-12'>
+        { isPending ? 
+            'loading...'
+            :
+          'load data'
+        }
+        </div>
     );
 };
 
