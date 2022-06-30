f1f3957619ac8e38801fc6d238197ad9b34e5b76
Azril Nazli Alias <sales@stream.my>
2022-06-30 14:22:45 +0800

    MY PROPOSAL - disable delete

--- src/components/MyProposal/FormDataTypeB.js
+++ src/components/MyProposal/FormDataTypeB.js
@@ -456,7 +456,7 @@ const Detail = ({setDestroyed,proposal,tender,created_at}) => {
                 <div>
                   <div className="d-flex flex-row">
                       {/* <div className=""><EditProposal /></div>        */}
-                      <div className="ms-2"><DeleteProposal /></div>     
+                      {/* <div className="ms-2"><DeleteProposal /></div>      */}
                       <div className="ms-auto align-self-center">Applied on <strong>{date}</strong></div>               
                   </div>
                 </div>                     
