13c2f9dea00abc5c5270be102c4dbb3d35ecb159
Azril Nazli Alias <sales@stream.my>
2022-06-30 11:10:30 +0800

    stash

--- src/components/Tender/ErrorMsg.js
+++ src/components/Tender/ErrorMsg.js
@@ -11,19 +11,26 @@ const ErrorMsg = ({title,message}) => {
         //         </div>
         // </div>
         <div className="alert alert-danger d-flex align-items-center" role="alert">
-           
-            <div>
-            <i class="fas fa-exclamation"></i> <strong>{title}</strong> 
-            {/* {message} */}
-            <p>Assalamualaikum dan Salam Sejahtera,</p>
-
-            <p>Syarikat masih gagal untuk memenuhi keperluan pendaftaran, dan status masih belum berjaya berdaftar sebagai pembekal yang berdaftar di dalam Sistem RTM TVCMS.</p>
-
-            <p>Sila rujuk kepada Urusetia TVCMS.</p>
-
-            <p>Sekian, terima kasih.</p>
-            </div>
+                
+                <span><strong><i class="fas fa-exclamation"></i> {title}</strong> </span>             
+       
+                <span className='ms-1'>{message}</span>            
+        
         </div>
+        // <div className="alert alert-danger d-flex align-items-center" role="alert">
+           
+        //     <div>
+        //     <i class="fas fa-exclamation"></i> <strong>{title}</strong> 
+        //     {/* {message} */}
+        //     <p>Assalamualaikum dan Salam Sejahtera,</p>
+
+        //     <p>Syarikat masih gagal untuk memenuhi keperluan pendaftaran, dan status masih belum berjaya berdaftar sebagai pembekal yang berdaftar di dalam Sistem RTM TVCMS.</p>
+
+        //     <p>Sila rujuk kepada Urusetia TVCMS.</p>
+
+        //     <p>Sekian, terima kasih.</p>
+        //     </div>
+        // </div>
     );
 };
 
