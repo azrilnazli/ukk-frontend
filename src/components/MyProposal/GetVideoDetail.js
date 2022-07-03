81c20c3473bd54492888c754314774a1ca5fb415
Azril Nazli Alias <sales@stream.my>
2022-07-04 02:17:57 +0800

    VIDEO - add direct link for original video

--- src/components/MyProposal/GetVideoDetail.js
+++ src/components/MyProposal/GetVideoDetail.js
@@ -56,6 +56,7 @@ const GetVideoDetail = ({video_id}) => {
                 <pre>Original codec : {video.format}</pre>
                 <pre>Original size : {video.uploaded_size}</pre>
                 <pre>Video Length : {video.length}</pre>
+                <pre>Original Video : <a target="_blank" href="http://202.165.14.246:8080/{{ $row->id }}/original.mp4">{video.original_filename}</a></pre>
                 <pre>Encoding status :&nbsp;
                   { video.is_failed ? 
                       'encoding was failed ( suggestion : Please re-encode the video )' 
