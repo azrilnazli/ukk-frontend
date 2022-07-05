e3cfe85b3a6001784bfb59644b96476f683e4b8f
Azril Nazli Alias <sales@stream.my>
2022-07-05 11:08:30 +0800

    TenderDetail/Detail.js - change wording

--- src/components/TenderDetail/Detail.js
+++ src/components/TenderDetail/Detail.js
@@ -19,7 +19,7 @@ const Detail = ({key,tender}) => {
     return (
             <div key={key} className="card mt-3">
                 <div className="card-header">
-                <h2>{tender.programme_category} - {tender.programme_code}</h2>
+                <h2>{tender.programme_category} [{tender.programme_code}]</h2>
                 </div>
 
                 <div className="card-body">
